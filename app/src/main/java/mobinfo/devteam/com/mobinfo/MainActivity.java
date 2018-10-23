ac08832952cb25c03f753f103acbf81df075e55d
mohammedelshafey12 <41911027+mohammedelshafey12@users.noreply.github.com>
2018-10-23 15:37:54 +0200

    Initial commit

---
+++ app/src/main/java/mobinfo/devteam/com/mobinfo/MainActivity.java
@@ -0,0 +1,40 @@
+package mobinfo.devteam.com.mobinfo;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String  details =  "VERSION.RELEASE : "+ Build.VERSION.RELEASE
+                +"\nVERSION.INCREMENTAL : "+Build.VERSION.INCREMENTAL
+                +"\nVERSION.SDK.NUMBER : "+Build.VERSION.SDK_INT
+                +"\nBOARD : "+Build.BOARD
+                +"\nBOOTLOADER : "+Build.BOOTLOADER
+                +"\nBRAND : "+Build.BRAND
+                +"\nCPU_ABI : "+Build.CPU_ABI
+                +"\nCPU_ABI2 : "+Build.CPU_ABI2
+                +"\nDISPLAY : "+Build.DISPLAY
+                +"\nFINGERPRINT : "+Build.FINGERPRINT
+                +"\nHARDWARE : "+Build.HARDWARE
+                +"\nHOST : "+Build.HOST
+                +"\nID : "+Build.ID
+                +"\nMANUFACTURER : "+Build.MANUFACTURER
+                +"\nMODEL : "+Build.MODEL
+                +"\nPRODUCT : "+Build.PRODUCT
+                +"\nSERIAL : "+Build.SERIAL
+                +"\nTAGS : "+Build.TAGS
+                +"\nTIME : "+Build.TIME
+                +"\nTYPE : "+Build.TYPE
+                +"\nUNKNOWN : "+Build.UNKNOWN
+                +"\nUSER : "+Build.USER ;
+        TextView textView = new TextView(this);
+        textView.setText(details);
+        setContentView(textView);
+    }
+}
